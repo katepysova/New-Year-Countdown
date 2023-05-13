78c47402d5c17cf8b0a1055ebbc6fd572a39b96e
Kateryna <katepysova97@gmail.com>
2023-05-14 00:31:06 +0200

    Basic

---
+++ README.md
@@ -0,0 +1,31 @@
+# Memory Cards
+
+Memory Cards App made with Pure JS, CSS and HTML. [Demo](https://katepysova.github.io/Memory-Cards/).
+
+## Installation
+
+1. Clone this repo or downloand zip:
+
+   `git@github.com:katepysova/Memory-Cards.git`
+
+2. Change your current directory to this project directory.
+
+3. `npm install`
+
+4. `npm run prepare` - to install git pre-commit hook. Does not work not in a git repository.
+
+5. `git add .husky/pre-commit` - to add git pre-commit hook.
+
+## How to run
+
+`npm run dev` - to start the app on the localhost.
+
+## Additional
+
+`npm run format` - to format and lint the code.
+
+### Stack
+
+- CSS3;
+- HTML5;
+- JS (ES6);
---
+++ src/script.js
@@ -0,0 +1,58 @@
+const daysEl = document.querySelector("#days");
+const minutesEl = document.querySelector("#minutes");
+const hoursEl = document.querySelector("#hours");
+const secondsEl = document.querySelector("#seconds");
+
+const currentYear = new Date().getFullYear();
+const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
+
+const snowContainer = document.querySelector(".snow-container");
+
+const snowContent = ["&#10052", "&#10053", "&#10054"];
+const snowColors = ["#ffffff", "#ecfffd", "#a0e6ec", "#94f2f4"];
+
+const addForwardZero = (value) => (value < 10 ? `0${value}` : `${value}`);
+
+function update() {
+  const currentTime = new Date();
+  const diff = newYearTime - currentTime;
+  const days = Math.floor(diff / 1000 / 60 / 60 / 24);
+  const hours = Math.floor(diff / 1000 / 60 / 60) % 24;
+  const minutes = Math.floor(diff / 1000 / 60) % 60;
+  const seconds = Math.floor(diff / 1000) % 60;
+
+  daysEl.textContent = addForwardZero(days);
+  hoursEl.innerText = addForwardZero(hours);
+  minutesEl.innerText = addForwardZero(minutes);
+  secondsEl.innerText = addForwardZero(seconds);
+}
+
+const getRandomInteger = (min = 0, max = 100) =>
+  Math.floor(Math.random() * (max - min + 1)) + min;
+
+const getRandomStyles = () => {
+  const top = getRandomInteger();
+  const left = getRandomInteger();
+  const dur = getRandomInteger(4, 24);
+  const size = getRandomInteger(2, 88);
+  return ` 
+    top: -${top}%; 
+    left: ${left}%; 
+    font-size: ${size}px; 
+    animation-duration: ${dur}s; 
+    color: ${snowColors[getRandomInteger(0, snowColors.length - 1)]}
+    `;
+};
+
+const createSnow = (num) => {
+  for (let i = 0; i < num; i++) {
+    const snowFlake = document.createElement("div");
+    snowFlake.className = "snowflake parallax-item";
+    snowFlake.style.cssText = getRandomStyles();
+    snowFlake.innerHTML = snowContent[getRandomInteger(0, 2)];
+    snowContainer.append(snowFlake);
+  }
+};
+
+setInterval(update, 1000);
+createSnow(130);
